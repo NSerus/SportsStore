53317a9035c9987968634681ed7bf6cb3de0c120
NSerus <nunoworkingserus@gmail.com>
2020-11-06 10:08:30 +0000

    Added Database model item     And made some classes

---
+++ SportsStore/Models/SportsStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class SportsStoreDbContext : DbContext
+    {
+        public SportsStoreDbContext(DbContextOptions options) : base(options)
+        {}
+        public DbSet<Product> Products { get; set; }
+    }
+}
