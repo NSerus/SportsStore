d89d69a2ed864d2c22da18293df4fcef8985d511
NSerus <nunoworkingserus@gmail.com>
2020-11-06 10:35:57 +0000

    created EntityFrameWorkRepo changed json setting to connect to the DB repo added repo to startup and changed AddTransient

---
+++ SportsStore/Models/EntityFrameWorkRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EntityFrameWorkRepository : ISportsStoreRepository
+    {
+        private SportsStoreDbContext context;
+        public EntityFrameWorkRepository(SportsStoreDbContext context){
+            this.context = context;            
+        }
+
+        public IEnumerable<Product> Products=> context.Products;
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -31,12 +31,18 @@ namespace SportsStore
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<SportsStoreDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("SportsStoreConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
             
-            services.AddTransient<ISportsStoreRepository, FakeRepository>();
+            services.AddTransient<ISportsStoreRepository, EntityFrameWorkRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
