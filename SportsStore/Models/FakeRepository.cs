12ad2d07177d9adfd796204f4160249ff83c673a
NSerus <nunoworkingserus@gmail.com>
2020-11-06 08:59:56 +0000

    Made fake repo

---
+++ SportsStore/Models/FakeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeRepository : ISportsStoreRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product {ProductId = 1,
+                Name = "Soccar Ball",
+                Price = 25,
+                Category = "Soccer"  };
+             new Product {ProductId = 2,
+                 Name = "Surf Board",
+                 Price = 302,
+                 Category = "Aquatic sports"  };
+             new Product {ProductId = 3,
+                 Name = "Running Shoes",
+                 Price = 95,
+                 Category = "Running"  };
+        };
+    }
+}
